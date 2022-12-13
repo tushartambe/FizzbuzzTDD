b7922df3098c94e9aa5e5445945fdbeb3b3b8b83
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2022-12-13 16:46:00 +0530

    Add failing test for buzz use case

--- src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
+++ src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
@@ -1,11 +1,10 @@
 package com.thoughtworks.practice;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzbuzzTest {
     private Fizzbuzz fizzbuzz;
@@ -19,6 +18,15 @@ class FizzbuzzTest {
     @ValueSource(strings = {"3", "9"})
     void shouldReturnFizzForNumberDivisibleBy3(String input) {
         String expected = "fizz";
+        String actual = fizzbuzz.getFizzOrBuzz(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"5", "10"})
+    void shouldReturnBuzzForNumberDivisibleBy5(String input) {
+        String expected = "buzz";
         String actual = fizzbuzz.getFizzOrBuzz(input);
 
         assertEquals(expected, actual);

57555a93e5b0a4ef3dc37d80aae27e6b48a3ecb7
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2022-12-13 16:46:38 +0530

    Add implementation for buzz use case

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -6,6 +6,10 @@ public class Fizzbuzz {
         if (parsedNumber % 3 == 0) {
             return "fizz";
         }
+
+        if (parsedNumber % 5 == 0) {
+            return "buzz";
+        }
         
         return "not fizzy or buzzy";
     }

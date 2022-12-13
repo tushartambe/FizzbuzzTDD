626dac49f8c1188ae8c38dd5cd5e09b59d33263d
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2022-12-13 17:29:59 +0530

    Refactor code for fizzbuzz use case

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -3,14 +3,16 @@ package com.thoughtworks.practice;
 public class Fizzbuzz {
     public String getFizzOrBuzz(String s) {
         int parsedNumber = Integer.parseInt(s);
+        
+        String result = "";
         if (parsedNumber % 3 == 0) {
-            return "fizz";
+            result += "fizz";
         }
 
         if (parsedNumber % 5 == 0) {
-            return "buzz";
+            result += "buzz";
         }
-        
-        return "not fizzy or buzzy";
+
+        return result;
     }
 }
